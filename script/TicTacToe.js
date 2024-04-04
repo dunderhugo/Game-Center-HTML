3720c0c7a7c3a9784a3ff6de221dae68b8c0e795
dunderhugo <forssellhugo@gmail.com>
2024-04-04 23:10:24 +0200

    Added tic tac toe page

---
+++ script/TicTacToe.js
@@ -0,0 +1,29 @@
+
+
+
+var s1 = document.getElementById("squareOne");
+var s2 = document.getElementById("squareTwo");
+var s3 = document.getElementById("squareThree");
+var s4 = document.getElementById("squareFour");
+var s5 = document.getElementById("squareFive");
+var s6 = document.getElementById("squareSix");
+var s7 = document.getElementById("squareSeven");
+var s8 = document.getElementById("squareEight");
+var s9 = document.getElementById("squareNine");
+
+
+s1.onclick = function(){
+    if(s1.disable) alert("Can not put tile on twice")
+    else {
+        s1.disable = true;
+        s1.style.backgroundColor = "";
+        console.log("test");
+    }
+}
+
+ function squareValue(value, value2){
+    if (value = true){
+
+    }
+ }
+
