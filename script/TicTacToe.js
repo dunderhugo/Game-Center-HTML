cd9588bc82c7d2749767ca73fcbdf3691dabd924
dunderhugo <forssellhugo@gmail.com>
2024-04-08 15:33:50 +0200

    Refactor/change name on the tiles
    Fewer lines of code

--- script/TicTacToe.js
+++ script/TicTacToe.js
@@ -1,26 +1,22 @@
-var s1 = document.getElementById("squareOne");
-var s2 = document.getElementById("squareTwo");
-var s3 = document.getElementById("squareThree");
-var s4 = document.getElementById("squareFour");
-var s5 = document.getElementById("squareFive");
-var s6 = document.getElementById("squareSix");
-var s7 = document.getElementById("squareSeven");
-var s8 = document.getElementById("squareEight");
-var s9 = document.getElementById("squareNine");
+var squareArr = [];
+for (var i = 1; i <= 9; i++){
+    squareArr.push(document.getElementById("square" + i));
+}
+
 var resetGame = document.getElementById("resetGame");
 
 
 var playerTurn = 0;
 
-s1.onclick = function() {playerMove(s1)};
-s2.onclick = function() {playerMove(s2)};
-s3.onclick = function() {playerMove(s3)};
-s4.onclick = function() {playerMove(s4)};
-s5.onclick = function() {playerMove(s5)};
-s6.onclick = function() {playerMove(s6)};
-s7.onclick = function() {playerMove(s7)};
-s8.onclick = function() {playerMove(s8)};
-s9.onclick = function() {playerMove(s9)};
+squareArr[0].onclick = function() {playerMove(squareArr[0])};
+squareArr[1].onclick = function() {playerMove(squareArr[1])};
+squareArr[2].onclick = function() {playerMove(squareArr[2])};
+squareArr[3].onclick = function() {playerMove(squareArr[3])};
+squareArr[4].onclick = function() {playerMove(squareArr[4])};
+squareArr[5].onclick = function() {playerMove(squareArr[5])};
+squareArr[6].onclick = function() {playerMove(squareArr[6])};
+squareArr[7].onclick = function() {playerMove(squareArr[7])};
+squareArr[8].onclick = function() {playerMove(squareArr[8])};
 
 function playerMove(tile){
     var playerTurnDisplay = document.getElementById("playerTurnDisplay");
