bde7d93b50924f9fe9de73d584219d9434708cd6
dunderhugo <forssellhugo@gmail.com>
2024-04-15 14:37:44 +0200

    Removed semicolons

--- script/TicTacToe.js
+++ script/TicTacToe.js
@@ -13,7 +13,7 @@ for (let i = 1; i <= 9; i++){
 var resetGame = document.getElementById("resetGame");
 function gameReset(){
     location.reload();
-};
+}
 resetGame.addEventListener("click",gameReset);
 
 // Adds EventListener to each button
@@ -21,7 +21,7 @@ for(let i = 0; i < squareArr.length; i++){
     squareArr[i].addEventListener('click', function(){
         playerMove(squareArr[i]);
     });
-};
+}
 
 var playerTurn = 0;
 
@@ -46,7 +46,7 @@ function playerMove(tile){
     if (winner = false){
        setTimeout(checkIfTie, 10);
     }
-};
+}
 
 
 function checkIfTie(){
@@ -54,17 +54,16 @@ function checkIfTie(){
     for(let i = 0; i < 10; i++){
         if(squareArr[i] && squareArr[i].textContent !== ""){
             placedTies++;
-        };
-    };
+        }
+    }
     if (placedTies === 9){
-        window.alert("Its a tie :(((");
-    };
-};
+        window.alert("Its a tie.");
+    }
+}
 
 //TODO: Refactor
 //TODO: Restart game if someone wins
-
-/* Checks if winning */
+/* Checks if someone wins */
 function checkWinnerFunc(checkOne, checkTwo, checkThree){
     if(checkOne.textContent !==""){
         if ((checkOne.textContent === checkTwo.textContent) && 
@@ -78,9 +77,9 @@ function checkWinnerFunc(checkOne, checkTwo, checkThree){
             },10);
             winner = true;
             return;
-        };
-    };
-};
+        }
+    }
+}
 function threeInARowCheck(){
     checkWinnerFunc(squareArr[4], squareArr[1],squareArr[7])
     checkWinnerFunc(squareArr[4], squareArr[3],squareArr[5])
@@ -90,4 +89,10 @@ function threeInARowCheck(){
     checkWinnerFunc(squareArr[0], squareArr[3],squareArr[6])
     checkWinnerFunc(squareArr[8], squareArr[7],squareArr[6])
     checkWinnerFunc(squareArr[8], squareArr[5],squareArr[2])
-};
+}
+
+function tileBtnDisable(tileDisable){
+    for (let i = 0; i < tileDisable.length; i++){
+
+    }
+}
