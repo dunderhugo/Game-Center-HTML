813369ec73ca28fd4fbf9837483f5e5478d8cb75
dunderhugo <forssellhugo@gmail.com>
2024-04-18 12:59:02 +0200

    Changed order of rules in HTML, changed id to exitRuleBtn,

--- script/memory.js
+++ script/memory.js
@@ -1,8 +1,15 @@
 document.getElementById("rulesBtn").addEventListener("click", function(){
     var rulesDiv = document.getElementById("rulesDiv");
-    if (rulesDiv.style.display === "none" || rulesDiv.style.display === ""){
+    if (rulesDiv.style.display === "none" || rulesDiv.style.display === "")
+    {
         rulesDiv.style.display = "block";
-    } else {
+    } 
+    else 
+    {
         rulesDiv.style.display = "none";
     }
-})
+});
+
+document.getElementById("exitRulesBtn").addEventListener("click", function(){
+    rulesDiv.style.display = "none";
+});
